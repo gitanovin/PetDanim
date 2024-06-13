c7805d2f7481a3f30e569d8f3d6d15294f06a8eb
Mahboub Hosseinzadeh <91733981+mahboubhosseinzadeh@users.noreply.github.com>
2024-06-13 22:42:17 +0330

    update

---
+++ pages/archive/tags/single/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-slate-100 dark:bg-dark-900">
+    <TagPage />
+  </div>
+</template>
+
+<script setup>
+import TagPage from "@/components/TemplateParts/Category/categoryPage.vue";
+</script>
--- pages/archive/tags/tags/index.vue
+++ pages/archive/tags/tags/index.vue
@@ -3,7 +3,7 @@
         <BlogSection class="dark:!bg-dark-900/30 bg-gray-50 py-16 lg:py-16">
             <HeaderSection />
             <Grid class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
-                <contentCategory />
+                <contentTag/>
             </Grid>
         </BlogSection>
     </div>
@@ -13,5 +13,5 @@
 import BlogSection from "@/components/TemplateParts/Section/BlogSection.vue";
 import HeaderSection from "@/components/TemplateParts/Header/header.vue";
 import Grid from "@/components/TemplateParts/Grid/Grid.vue";
-import contentCategory from "@/components/TemplateParts/contentCategory.vue";
+import contentTag from "@/components/TemplateParts/contentTag.vue";
 </script>
